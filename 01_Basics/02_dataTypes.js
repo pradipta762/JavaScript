5e73371abd07dcc76d331b200496a1e2a7fa188c
Pradipta Kumar Dash <116185668+pradipta762@users.noreply.github.com>
2023-06-21 14:44:56 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) //we are using node js, not browser
+
+console.log(3 
+    + 3)    //code readability should be high 
+
+
+console.log("Pradipta")
+
+let name = "Pradipta"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value(Representation of empty value)
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined)
