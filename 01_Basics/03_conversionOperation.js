4bb66349582bb87b2439e66198886c2a82cebfda
Pradipta Kumar Dash <116185668+pradipta762@users.noreply.github.com>
2023-06-22 01:59:33 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "pradipta"
+
+//console.log(typeof(score))
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber) //NaN :- Not a Number
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1: false => 0
+// null => 0
+// undifined => NaN
+
+let isLoggedIn = "2"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// "pradipta" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
