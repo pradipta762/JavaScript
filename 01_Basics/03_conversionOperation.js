8a40f221e00f77601c9affae42f2e454f6d260bb
Pradipta Kumar Dash <116185668+pradipta762@users.noreply.github.com>
2023-06-23 07:19:35 +0000

    Conversion Operation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -17,7 +17,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "2"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false
 // "" => false
@@ -25,5 +25,40 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ***************** Operation *****************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(5 * 2);
+// console.log(10 / 2);
+// console.log(5 % 2);
+
+let str1 = "Hello"
+let str2 = " Pradipta"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+ true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
